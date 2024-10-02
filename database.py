e87f8fbf01e08a35a5dcf19da067366034a0673e
Azamat <azamatovuser@gmail.com>
2024-10-02 07:49:52 +0500

    initial commit

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+SQLALCHEMY_URL = "sqlite:///./database.db"
+
+engine = create_engine(SQLALCHEMY_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
